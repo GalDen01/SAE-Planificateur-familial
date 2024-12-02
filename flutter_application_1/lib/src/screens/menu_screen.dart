c0b96b728fd735434b073d92c5a9f8263ac25b31
Elian Ollivier <elian.ollivier@etu.unilim.fr>
2024-12-02 16:05:51 +0100

    ajout nouveau menu principal premiere version qui correspond a la maquette

---
+++ flutter_application_1/lib/src/screens/menu_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../widgets/weather_card.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF616161), // Gris foncé
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'Menu principal',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const WeatherCard(),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFFB0BEC5), // Gris clair
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+            ),
+            onPressed: () {},
+            child: const Text('Famille #3'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFFB0BEC5),
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+            ),
+            onPressed: () {},
+            child: const Text('Famille #4'),
+          ),
+        ],
+      ),
+    );
+  }
+}
