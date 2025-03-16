3d2e8e2480f648eedd65972f0ca1b7c9f56bd383
elianollivier <elian.ollivier@etu.unilim.fr>
2025-03-16 20:27:40 +0100

    ajout en commentaire de la commande de build apk

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 // commande pour run flutter run -d chrome --web-port=1234 --web-hostname=localhost --dart-define=SUPABASE_URL=https://jyeysdflargvkirkjmbe.supabase.co --dart-define=SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5ZXlzZGZsYXJndmtpcmtqbWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUwMzIxMDUsImV4cCI6MjA1MDYwODEwNX0.BlHBvxXMooEB32CsVv_h67P-DmoHAyWrsH9kV5iRuL4
 // commande pour run flutter run --dart-define=SUPABASE_URL=https://jyeysdflargvkirkjmbe.supabase.co --dart-define=SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5ZXlzZGZsYXJndmtpcmtqbWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUwMzIxMDUsImV4cCI6MjA1MDYwODEwNX0.BlHBvxXMooEB32CsVv_h67P-DmoHAyWrsH9kV5iRuL4
+// commande pour build flutter build apk --release --dart-define=SUPABASE_URL=https://jyeysdflargvkirkjmbe.supabase.co --dart-define=SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5ZXlzZGZsYXJndmtpcmtqbWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUwMzIxMDUsImV4cCI6MjA1MDYwODEwNX0.BlHBvxXMooEB32CsVv_h67P-DmoHAyWrsH9kV5iRuL4
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
